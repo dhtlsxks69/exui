31f3096097361e2ec6f213663a7f1e9b85214ab5
dhtlsxks69 <dhtlsxks69@naver.com>
2019-08-08 18:22:44 +0900

    19/08/08

---
+++ myplugin.js
@@ -0,0 +1,9 @@
+$.fn.backYellow = function(){
+    this.css('background-color', 'yellow');
+    return this;
+}
+
+$.fn.bgColor = function(option){
+    this.css('background-color', option);
+    return this;
+}
